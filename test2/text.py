fbc74e44184feb9fd5052b88ccab3bd2cef64c48
Marvin Martínez <marvin1ronal@gmail.com>
2024-11-01 16:44:36 -0600

    :sparkles: finish semester

---
+++ test2/text.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+print(tf.__version__)
+
+print("GPU Available: ", len(tf.config.list_physical_devices('GPU')))
+
+device_lib.list_local_devices()
+
+# import tensorflow as tf
+# mnist=tf.keras.datasets.mnist
+# (x_train,y_train),(x_test,y_test)=mnist.load_data()
+# x_train,x_test=x_train/255.0,x_test/255.0
+# model=tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28,28)),
+#     tf.keras.layers.Dense(128,activation='relu'),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Dense(10,activation='softmax')
+# ])
+
+# model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+# model.fit(x_train,y_train,epochs=15)
+# model.evaluate(x_test,y_test,verbose=2)
